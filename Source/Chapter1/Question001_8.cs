829b96554f7cf5d0eedf9d0abd71837ca3caa9db
moriazat <moriazat@users.noreply.github.com>
2014-12-18 11:50:51 +0330

    Added Solution to Chapter 1 Question 8
    Solution to Chapter 1 Question 8 is added.

---
+++ Source/Chapter1/Question001_8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CrackingTheCodingInterview.Chapter1
+{
+	public static class Question001_8
+	{
+		public static bool IsRotation(string s1, string s2)
+		{
+			if (string.IsNullOrEmpty(s1) || (s1.Length != s2.Length))
+				return false;
+				
+			string concat = s1 + s1;
+			return IsSubstring(concat, s2);
+		}
+	
+		private static bool IsSubstring(string s1, string s2)
+		{
+			return s1.Contains(s2);
+		}
+	}
+}
---
+++ Tests/Chapter1Tests/Question001_8_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterview.Chapter1.Tests
+{
+	[TestClass]
+	public class Question001_8_Test
+	{
+		[TestMethod]
+		public void IsRotation_NullStrings_ReturnsFalse()
+		{
+			bool actual = Question001_8.IsRotation(null, null);
+			
+			Assert.IsFalse(actual);
+		}
+		
+		[TestMethod]
+		public void IsRotation_NotEquallyLongStrings_ReturnsFalse()
+		{
+			bool actual = Question001_8.IsRotation("MyName", "YourName");
+			
+			Assert.IsFalse(actual);
+		}
+		
+		[TestMethod]
+		public void IsRotation_EquallyLongNonRotationStrings_ReturnsFalse()
+		{
+			bool actual = Question001_8.IsRotation("staff", "stuff");
+			
+			Assert.IsFalse(actual);
+		}
+		
+		[TestMethod]
+		public void IsRotaton_RotatedStrings_ReturnsTrue()
+		{
+			bool actual = Question001_8.IsRotation("tapwater", "watertap");
+			
+			Assert.IsTrue(actual);
+		}
+	}
+}
