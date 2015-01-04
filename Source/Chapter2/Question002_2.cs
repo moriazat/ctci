cf6dd7510ee6c6f1413ddb454605f21457e53746
moriazat <moriazat@users.noreply.github.com>
2015-01-04 18:48:48 +0330

    Added solutions to Chapter 2 Question 2
    Solutions to Chapter 2 Question 2 are added along with the unit tests
    written for them.

---
+++ Source/Chapter2/Question002_2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using CrackingTheCodingInterview.DataStructures;
+
+namespace CrackingTheCodingInterview.Chapter2
+{
+	public static class Question002_2
+	{
+		public static SinglyLinkedListNode<T> FindKthElementToLastUsingLength<T>(SinglyLinkedList<T> list, int k)
+		{
+			if (k > list.Count || k <= 0)
+				return null;
+			
+			int index = list.Count - k;
+			
+			SinglyLinkedListNode<T> node = list.Head;
+			
+			for (int i = 0; i < index; i++)
+				node = node.Next;
+			
+			return node;
+		}
+		
+		public static SinglyLinkedListNode<T> FindKthElementToLastUsingPointers<T>(SinglyLinkedList<T> list, int k)
+		{
+			if (k > list.Count || k <= 0)
+				return null;
+				
+			SinglyLinkedListNode<T> first, second;
+			
+			first = list.Head;
+			second = first;
+			
+			// set the pointers k nodes apart
+			for (int i = 1; i < k; i++)
+				second = second.Next;
+			
+			// move both pointers (with keeping the gap) till the second one reaches the end
+			if (second != list.Tail)
+			{
+				while (second != list.Tail)
+				{
+					first = first.Next;
+					second = second.Next;
+				}
+			}
+			
+			return first;
+		}
+		
+		public static SinglyLinkedListNode<T> FindKthElementToLastRecursively<T>(SinglyLinkedList<T> list, int k)
+		{
+			if (k > list.Count  || k <= 0)
+				return null;
+				
+			return Find(list.Head, ref k);
+		}
+		
+		private static SinglyLinkedListNode<T>  Find<T>(SinglyLinkedListNode<T> node, ref int k)
+		{
+			if (node.Next == null)
+			{
+				// start counting down
+				k--;
+				return null;
+			}
+			
+			SinglyLinkedListNode<T> element = Find(node.Next, ref k);
+			
+			k--;
+			
+			if (k == 0)
+				return node;
+			
+			return element;
+		}
+	}
+}
---
+++ Tests/Chapter2Tests/Question002_2_Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrackingTheCodingInterview.DataStructures;
+
+namespace CrackingTheCodingInterview.Chapter2.Tests
+{
+	[TestClass]
+	public class Question002_2_Test
+	{
+		[TestMethod]
+		public void FindElemingUsingLength_ListShorterThanK_ReturnsNull()
+		{
+			int[] initialArray = {1,2,3,4,5};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+			SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastUsingLength(list, 6);
+			
+			Assert.AreEqual(null, actual);
+		}
+		
+		[TestMethod]
+		public void FindElementUsingLength_ListHavingKthElement_ReturnsKthElement()
+		{
+			int[] initialArray = {0,1,2,3,4,5,6,7,8,9};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+            SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastUsingLength(list, 4);
+			
+			Assert.AreEqual(6, actual.Data);
+		}
+		
+		[TestMethod]
+		public void FindElemingUsingPointers_ListShorterThanK_ReturnsNull()
+		{
+			int[] initialArray = {1,2,3,4,5};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+            SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastUsingPointers(list, 6);
+			
+			Assert.AreEqual(null, actual);
+		}
+		
+		[TestMethod]
+		public void FindElementUsingPointers_ListHavingKthElement_ReturnsKthElement()
+		{
+			int[] initialArray = {0,1,2,3,4,5,6,7,8,9};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+            SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastUsingPointers(list, 4);
+			
+			Assert.AreEqual(6, actual.Data);
+		}
+		
+		[TestMethod]
+		public void FindElemingUsingRecursively_ListShorterThanK_ReturnsNull()
+		{
+			int[] initialArray = {1,2,3,4,5};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+            SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastRecursively(list, 6);
+			
+			Assert.AreEqual(null, actual);
+		}
+		
+		[TestMethod]
+		public void FindElementUsingRecursively_ListHavingKthElement_ReturnsKthElement()
+		{
+			int[] initialArray = {0,1,2,3,4,5,6,7,8,9};
+			SinglyLinkedList<int> list = new SinglyLinkedList<int>(initialArray);
+            SinglyLinkedListNode<int> actual = Question002_2.FindKthElementToLastRecursively(list, 4);
+			
+			Assert.AreEqual(6, actual.Data);
+		}
+	}
+}
