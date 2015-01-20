402caeb905812f231e9bffa2676ac21179461c39
moriazat <moriazat@users.noreply.github.com>
2015-01-20 18:52:25 +0330

    Added Solutions to Chapter 3 Question 1
    Solutions to Chapter 3 Question is added along with unit tests written
    for them.

---
+++ Source/Chapter3/Question003_1.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview.Chapter3
+{
+	public class Question003_1
+	{
+		public class FixedSizeStack<T>
+		{
+			private int id;
+			private T[] store;
+			private int topIndex;
+			private int size;
+			private int lowerBoundary;
+			
+			public FixedSizeStack(T[] storageArray, int id)
+			{
+				Initialize(storageArray, id);
+			}
+			
+			public FixedSizeStack(T[] storageArray, int id, T[] initialArray)
+			{
+				Initialize(storageArray, id);
+				
+				foreach (T t in initialArray)
+					Push(t);
+			}
+			
+			public int ID
+			{
+				get { return this.id; }
+			}
+			
+			public T Pop()
+			{
+				if (topIndex < lowerBoundary)
+					throw new InvalidOperationException("Stack is empty.");
+				
+				T element = store[topIndex];
+				topIndex--;
+				
+				return element;
+			}
+			
+			public void Push(T item)
+			{
+				if (topIndex == lowerBoundary + size - 1)
+					throw new InvalidOperationException("Stack is full.");
+					
+				topIndex++;
+				store[topIndex] = item;
+			}
+			
+			private void Initialize(T[] array, int id)
+			{
+				this.id = id;
+				this.store = array;
+				this.size = store.Length / 3;
+				this.lowerBoundary = id * size;
+                this.topIndex = lowerBoundary - 1;
+			}
+		}
+		
+		public class DynamicSizedStack<T>
+		{
+			public interface IStore<T>
+			{
+				void Add(int id, T item);
+				
+				T RemoveAt(int id, int index);
+				
+				T ItemAt(int id, int index);
+				
+				int GetSize(int id);
+			}
+			
+			public abstract class DynamicStoreBase<T> : IStore<T>
+			{
+				protected T[] array;
+				
+				public DynamicStoreBase(T[] array)
+				{
+					this.array = array;
+				}
+				
+				public abstract void Add(int id, T item);
+				
+				public abstract T RemoveAt(int id, int index);
+				
+				public abstract T ItemAt(int id, int index);
+				
+				public abstract int GetSize(int id);
+			}
+			
+			public class SimpleDynamicStore<T> : DynamicStoreBase<T>
+			{
+				private List<List<int>> itemsIndices;
+				private List<int> availableIndices;
+				
+				
+				public SimpleDynamicStore(T[] array) : base(array)
+				{
+					itemsIndices = new List<List<int>>();
+					availableIndices = new List<int>();
+					
+					for (int i = 0; i < array.Length; i++)
+						availableIndices.Add(i);
+				}
+				
+				public override void Add(int id, T item)
+				{
+					if (availableIndices.Count == 0)
+						throw new InvalidOperationException("The store is full.");
+					
+					if (id > itemsIndices.Count)
+						throw new ArgumentException(string.Format("The ID cannot be greater than {0}", itemsIndices.Count));
+					else if (id == itemsIndices.Count)
+						itemsIndices.Add(new List<int>());
+					
+					int index = availableIndices[0];
+					availableIndices.RemoveAt(0);
+					array[index] = item;
+					
+					itemsIndices[id].Add(index);
+				}
+				
+				public override T RemoveAt(int id, int index)
+				{
+					int idx = (itemsIndices[id])[index];
+					itemsIndices[id].RemoveAt(index);
+					T item = array[idx];
+					availableIndices.Add(idx);
+					
+					return item;
+				}
+				
+				public override T ItemAt(int id, int index)
+				{
+					int idx = (itemsIndices[id])[index];
+					return array[idx];
+				}
+				
+				public override int GetSize(int id)
+				{
+					return itemsIndices[id].Count;
+				}
+			}
+			
+			public class SegmentedDynamicStore<T> : DynamicStoreBase<T>
+			{
+				public class Segment
+				{
+					private int startPos;
+					private int endPos;
+					private int index;
+					private bool isFull;
+					private int size;
+					private const int INVALID_INDEX = -1;
+					private const int DEFAULT_INITIAL_SIZE = 2;
+					
+					public Segment(int startPosition, int initialSize)
+					{
+						this.startPos = startPosition;
+						this.endPos = startPos + initialSize - 1;
+						this.index = INVALID_INDEX;
+						this.isFull = false;
+						this.size = initialSize;
+					}
+					
+					public Segment(int startPosition) : this(startPosition, DEFAULT_INITIAL_SIZE)
+					{
+						// do nothing!
+					}
+					
+					public int Start
+					{
+						get { return this.startPos; }
+					}
+					
+					public int End
+					{
+						get { return this.endPos; }
+					}
+					
+					public int Index
+					{
+						get
+						{
+							return this.index; 
+						}
+						
+						set
+						{
+							if ( value > (size - 1) )
+								throw new ArgumentOutOfRangeException("The value is beyond the segment's ending position");
+							
+							if ( value == (size - 1) )
+								this.isFull = true;
+								
+							this.index = value - startPos; 
+						}
+					}
+
+					public int Size
+					{
+						get { return this.size; }
+					}
+					
+					public bool IsFull
+					{
+						get { return this.isFull; }
+					}
+					
+					public void Move(int newStart)
+					{
+						this.startPos = newStart;
+						this.endPos = startPos + this.size - 1;
+					}
+					
+					public void Expand(int newEnd)
+					{
+						this.endPos = newEnd;
+						this.size = endPos - startPos + 1;
+						
+						if (isFull)
+							isFull = false;
+					}
+					
+					public void Shrink(int newEnd)
+					{
+						this.endPos = newEnd;
+						this.size = endPos - startPos + 1;
+					}
+					
+					public int GetNewIndex()
+					{
+						if ( this.index == (size - 1) )
+							throw new InvalidOperationException("Segment is full.");
+						
+						this.index++;
+						
+						if ( this.index == (size - 1) )
+							this.isFull = true;
+						
+						return startPos + index;
+					}
+				}
+				
+				private List<Segment> segments;
+				
+				public SegmentedDynamicStore(T[] array) : base(array)
+				{
+					segments = new List<Segment>();
+				}
+				
+				public override void Add(int id, T item)
+				{
+					if ( id > (segments.Count + 1) )
+						throw new ArgumentException(string.Format("The current number of segments is {0} and the 'id' cannot be greater than {1}.", segments.Count, segments.Count + 1));
+				
+					if (segments.Count == 0 || id >= segments.Count)
+						CreateSegment();
+					
+					if (segments[id].IsFull)
+						ExpandSegment(id);
+					
+					int pos = segments[id].GetNewIndex();
+					array[pos] = item;
+				}
+				
+				public override T ItemAt(int id, int index)
+				{
+					if (index > segments[id].Size - 1)
+						throw new IndexOutOfRangeException();
+				
+					int pos = segments[id].Start + index;
+					return array[pos];
+				}
+				
+				public override T RemoveAt(int id, int index)
+				{
+					if (index > segments[id].Size - 1)
+						throw new IndexOutOfRangeException();
+					
+					int pos = segments[id].Start + index;
+					T element = array[pos];
+					ShiftElementsBack(id, pos+1);
+					
+					if ( (segments[id].Index) < (segments[id].Size / 3) )
+						ShrinkSegment(id);
+						
+					return element;
+				}
+				
+				public override int GetSize(int id)
+				{
+					return segments[id].Size;
+				}
+				
+				private void CreateSegment()
+				{
+					Segment seg; 
+					
+					if (segments.Count > 0)
+					{
+						int lastPos = segments[segments.Count-1].End;
+						seg = new Segment(lastPos+1);
+					}
+					else
+					{
+						seg = new Segment(0);
+					}
+					
+					segments.Add(seg);
+				}
+				
+				private void ExpandSegment(int id)
+				{
+					int lastPos = segments[segments.Count-1].End;
+					
+					if ( lastPos == (this.array.Length - 1) )
+						throw new InvalidOperationException(string.Format("The storage for ID {0} is full and cannot be expanded.", id));
+					
+					int diff = (array.Length - 1) - lastPos;
+					
+					if (diff > segments[id].Size)
+						diff = segments[id].Size;
+					
+					for(int i = segments.Count - 1; i > id; i--)
+					{
+						int start = segments[i].Start + diff;
+						MoveSegment(i, start);
+					}
+					
+					int end = segments[id].End;
+                    segments[id].Expand(end + diff);
+				}
+				
+				private void ShrinkSegment(int id)
+				{
+					int diff = segments[id].Size / 2;
+					
+					for (int i = id + 1; i < segments.Count - 1; i++)
+					{
+						int start = segments[i].Start - diff;
+						MoveSegment(i, start);
+					}
+				}
+				
+				private void MoveSegment(int id, int newStart)
+				{
+					// move the whole segment to the given new location
+					Segment seg = segments[id];
+					
+					for (int i = 0; i <= seg.Index; i++)
+						array[newStart + i] = array[seg.Start + i];
+					
+					seg.Move(newStart);
+				}
+				
+				private void ShiftElementsBack(int id, int index)
+				{
+					// shift all the elements starting at the given index one cell back
+					int newIndex = index - 1;
+					Segment seg = segments[id];
+					
+					for ( ; index <= seg.Index; index++, newIndex++)
+						array[seg.Start + newIndex] = array[seg.Start + index];
+						
+					// update the current index
+					seg.Index--;
+				}
+			}
+			
+			private IStore<T> store;
+			private int id;
+			private int topIndex;
+			private const int INVALID_INDEX = -1;
+			
+			
+			public DynamicSizedStack(IStore<T> theStore, int stackID)
+			{
+				Initialize(theStore, stackID);
+			}
+			
+			public DynamicSizedStack(IStore<T> theStore, int stackID, T[] initialArray)
+			{
+				Initialize(theStore, stackID);
+				
+				foreach (T t in initialArray)
+					Push(t);
+			}
+			
+			public int ID
+			{
+				get { return this.id; }
+			}
+			
+			public int Size
+			{
+				get { return this.store.GetSize(this.id); }
+			}
+			
+			public void Push(T item)
+			{
+				try
+				{
+					this.store.Add(this.id, item);
+				}
+				catch (InvalidOperationException ex)
+				{
+					throw new InvalidOperationException("The stack is full.");
+				}
+				
+				this.topIndex++;
+			}
+			
+			public T Pop()
+			{
+				if (topIndex == INVALID_INDEX)
+					throw new InvalidOperationException("The stack is empty.");
+					
+				T item = this.store.RemoveAt(this.id, topIndex);
+				this.topIndex--;
+
+                return item;
+			}
+			
+			private void Initialize(IStore<T> theStore, int stackID)
+			{
+				this.store = theStore;
+				this.id = stackID;
+				this.topIndex = INVALID_INDEX;
+			}
+		}
+	}
+}
---
+++ Tests/Chapter3Tests/Question003_1_Test.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrackingTheCodingInterview.TestHelpers;
+
+namespace CrackingTheCodingInterview.Chapter3.Tests
+{
+	public class Question003_1_Test
+	{
+		[TestClass]
+		public class FixedSizeStack_Test
+		{
+			[TestMethod]
+			public void Push_PushingItemIntoEachStack_ItemsArePushed()
+			{
+				int[] actual = new int[9];
+				int[] expected = {10,0,0,20,0,0,30,0,0};
+				Question003_1.FixedSizeStack<int> stack0 = new Question003_1.FixedSizeStack<int>(actual, 0);
+				Question003_1.FixedSizeStack<int> stack1 = new Question003_1.FixedSizeStack<int>(actual, 1);
+				Question003_1.FixedSizeStack<int> stack2 = new Question003_1.FixedSizeStack<int>(actual, 2);
+     
+				stack0.Push(10);
+				stack1.Push(20);
+				stack2.Push(30);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Push_PushingIntoAFullStack_ThrowsInvalidOperationException()
+			{
+				int[] actual = new int[9];
+				int[] initial = {1,2,3};
+				Question003_1.FixedSizeStack<int> stack0 = new Question003_1.FixedSizeStack<int>(actual, 0, initial);
+								
+				stack0.Push(10);
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Pop_PopingEmptyStack_ThrowsInvalidOperationException()
+			{
+				int[] store = new int[9];
+				Question003_1.FixedSizeStack<int> sut = new Question003_1.FixedSizeStack<int>(store, 0);
+				
+				sut.Pop();
+			}
+			
+			[TestMethod]
+			public void Pop_PopingEachStack_TopItemsArePopped()
+			{
+				int[] actual = new int[15];
+				int[] initial0 = {10,20,30,40};
+				int[] initial1 = {50,60,70,80};
+				int[] initial2 = {90,100,110,120};
+				int expected0 = 40;
+				int expected1 = 80;
+				int expected2 = 120;
+				Question003_1.FixedSizeStack<int> sut0 = new Question003_1.FixedSizeStack<int>(actual, 0, initial0);
+				Question003_1.FixedSizeStack<int> sut1 = new Question003_1.FixedSizeStack<int>(actual, 1, initial1);
+				Question003_1.FixedSizeStack<int> sut2 = new Question003_1.FixedSizeStack<int>(actual, 2, initial2);
+				
+				int actual0 = sut0.Pop();
+				int actual1 = sut1.Pop();
+				int actual2 = sut2.Pop();
+				
+				Assert.AreEqual(expected0, actual0);
+				Assert.AreEqual(expected1, actual1);
+				Assert.AreEqual(expected2, actual2);
+			}
+		}
+		
+		[TestClass]
+		public class SegmentedDynamicStore_Test
+		{
+			[TestMethod]
+			public void Add_AddingElementsToThreeSegments_ElementsAreAdded()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,0,3,0,5,0,0,0,0,0,0,0};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				
+				sut.Add(0, 2);
+				sut.Add(1, 3);
+				sut.Add(2, 5);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			public void Add_AddingElementsMoreThanTheCapacityOfASegment_ElementsAreAdded()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,3,4,5,6,0,0,0,10,0,0,0};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+                sut.Add(0, 6);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			public void Add_AddingANewSegmentAfterExpandingFirstSegment_NewItemIsAddedSuccessfully()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,3,4,5,6,0,20,0,10,0,20,0};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+				sut.Add(2, 20);
+                sut.Add(0, 6);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));				
+			}
+			
+			[TestMethod]
+			public void ItemAt_GettingItemAtSpecificIndex_ReturnsTheItem()
+			{
+				int[] array = new int[10];
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(array);
+				sut.Add(0, 1);
+				sut.Add(0, 2);
+				sut.Add(1, 4);
+				sut.Add(2,10);
+				sut.Add(0, 5);
+				sut.Add(0, 6);
+				
+				int actual = sut.ItemAt(0, 2);
+				
+				Assert.AreEqual(5, actual);
+			}
+			
+			[TestMethod]
+			public void RemoveAt_RemovingItemAtSpecificIndexFromMultiSegmentStore_ItemIsRemoved()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,3,5,6,6,0,20,0,10,0,20,0};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+				sut.Add(2, 20);
+                sut.Add(0, 6);
+				
+				// remove an item
+				int actualRemovedItem = sut.RemoveAt(0, 2);
+								
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+				Assert.AreEqual(4, actualRemovedItem);
+			}
+			
+			[TestMethod]
+			public void RemoveAt_AddingNewItemAfterRemovingOne_ItemIsAddedSuccessfully()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,3,5,6,7,0,20,0,10,0,20,0};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+				sut.Add(2, 20);
+                sut.Add(0, 6);
+				
+				// remove an item
+				int actualRemovedItem = sut.RemoveAt(0, 2);
+				// check if the current index of the segment is shifted back as well
+				sut.Add(0, 7);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+				Assert.AreEqual(4, actualRemovedItem);			
+			}
+		}
+		
+		[TestClass]
+		public class SimpleDynamicStore_Test
+		{
+			[TestMethod]
+			public void Add_AddingElementsByDifferentIDs_ElementsAreAdded()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,3,5,0,0,0,0,0,0,0,0,0};
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				
+				sut.Add(0, 2);
+				sut.Add(1, 3);
+				sut.Add(2, 5);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			public void ItemAt_GettingItemAtSpecificIndex_ReturnsTheItem()
+			{
+				int[] array = new int[10];
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(array);
+				sut.Add(0, 1);
+				sut.Add(0, 2);
+				sut.Add(1, 4);
+				sut.Add(2,10);
+				sut.Add(0, 5);
+				sut.Add(0, 6);
+				
+				int actual = sut.ItemAt(0, 2);
+				
+				Assert.AreEqual(5, actual);
+			}
+			
+			[TestMethod]
+			public void RemoveAt_RemovingItemAtSpecificIndexFromMultiIDStore_ItemIsRemoved()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,10,3,4,5,20,6,0,0,0,0,0};
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+				sut.Add(2, 20);
+                sut.Add(0, 6);
+				
+				// remove an item
+				int actualRemovedItem = sut.RemoveAt(0, 2);
+								
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+				Assert.AreEqual(4, actualRemovedItem);
+			}
+			
+			[TestMethod]
+			public void RemoveAt_AddingNewItemAfterRemovingOne_ItemIsAddedSuccessfully()
+			{
+				int[] actual = {0,0,0,0,0,0,0,0,0,0,0,0};
+				int[] expected = {2,10,3,4,5,20,6,7,0,0,0,0};
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> sut =
+                    new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				sut.Add(0, 2);
+				sut.Add(1, 10);
+				sut.Add(0, 3);
+				sut.Add(0, 4);
+				sut.Add(0, 5);
+				sut.Add(2, 20);
+                sut.Add(0, 6);
+				
+				// remove an item
+				int actualRemovedItem = sut.RemoveAt(0, 2);
+				// check if the current index of the segment is shifted back as well
+				sut.Add(0, 7);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+				Assert.AreEqual(4, actualRemovedItem);			
+			}
+		}		
+		
+		[TestClass]
+		public class DynamicSizedStack_WithSegmenetedDynamicStore_Test
+		{
+			[TestMethod]
+			public void Push_PushingItemIntoEachStack_ItemsArePushed()
+			{
+				int[] actual = new int[9];
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				int[] expected = {10,0,20,0,30,0,0,0,0};
+				Question003_1.DynamicSizedStack<int> stack0 = new Question003_1.DynamicSizedStack<int>(store, 0);
+				Question003_1.DynamicSizedStack<int> stack1 = new Question003_1.DynamicSizedStack<int>(store, 1);
+				Question003_1.DynamicSizedStack<int> stack2 = new Question003_1.DynamicSizedStack<int>(store, 2);
+     
+				stack0.Push(10);
+				stack1.Push(20);
+				stack2.Push(30);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Push_PushingIntoAFullStack_ThrowsInvalidOperationException()
+			{
+				int[] actual = new int[3];
+				int[] initial = {1,2,3};
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> stack0 = new Question003_1.DynamicSizedStack<int>(store, 0, initial);
+								
+				stack0.Push(10);
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Pop_PopingEmptyStack_ThrowsInvalidOperationException()
+			{
+				int[] actual = new int[9];
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> sut = new Question003_1.DynamicSizedStack<int>(store, 0);
+				
+				sut.Pop();
+			}
+
+			[TestMethod]
+			public void Pop_PopingEachStack_TopItemsArePopped()
+			{
+				int[] actual = new int[15];
+				int[] initial0 = {10,20,30,40};
+				int[] initial1 = {50,60,70,80};
+				int[] initial2 = {90,100,110,120};
+				int expected0 = 40;
+				int expected1 = 80;
+				int expected2 = 120;
+				Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SegmentedDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> sut0 = new Question003_1.DynamicSizedStack<int>(store, 0, initial0);
+				Question003_1.DynamicSizedStack<int> sut1 = new Question003_1.DynamicSizedStack<int>(store, 1, initial1);
+				Question003_1.DynamicSizedStack<int> sut2 = new Question003_1.DynamicSizedStack<int>(store, 2, initial2);
+				
+				int actual0 = sut0.Pop();
+				int actual1 = sut1.Pop();
+				int actual2 = sut2.Pop();
+				
+				Assert.AreEqual(expected0, actual0);
+				Assert.AreEqual(expected1, actual1);
+				Assert.AreEqual(expected2, actual2);
+			}
+		}
+		
+		[TestClass]
+		public class DynamicSizedStack_WithSiimpleDynamicStore_Test
+		{
+			[TestMethod]
+			public void Push_PushingItemIntoEachStack_ItemsArePushed()
+			{
+				int[] actual = new int[9];
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				int[] expected = {10,20,30,0,0,0,0,0,0};
+				Question003_1.DynamicSizedStack<int> stack0 = new Question003_1.DynamicSizedStack<int>(store, 0);
+				Question003_1.DynamicSizedStack<int> stack1 = new Question003_1.DynamicSizedStack<int>(store, 1);
+				Question003_1.DynamicSizedStack<int> stack2 = new Question003_1.DynamicSizedStack<int>(store, 2);
+     
+				stack0.Push(10);
+				stack1.Push(20);
+				stack2.Push(30);
+				
+				Assert.IsTrue(Equality.AreEqual(expected, actual));
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Push_PushingIntoAFullStack_ThrowsInvalidOperationException()
+			{
+				int[] actual = new int[3];
+				int[] initial = {1,2,3};
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> stack0 = new Question003_1.DynamicSizedStack<int>(store, 0, initial);
+								
+				stack0.Push(10);
+			}
+			
+			[TestMethod]
+			[ExpectedException(typeof(InvalidOperationException))]
+			public void Pop_PopingEmptyStack_ThrowsInvalidOperationException()
+			{
+				int[] actual = new int[9];
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> sut = new Question003_1.DynamicSizedStack<int>(store, 0);
+				
+				sut.Pop();
+			}
+
+			[TestMethod]
+			public void Pop_PopingEachStack_TopItemsArePopped()
+			{
+				int[] actual = new int[15];
+				int[] initial0 = {10,20,30,40};
+				int[] initial1 = {50,60,70,80};
+				int[] initial2 = {90,100,110,120};
+				int expected0 = 40;
+				int expected1 = 80;
+				int expected2 = 120;
+				Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int> store = 
+					new Question003_1.DynamicSizedStack<int>.SimpleDynamicStore<int>(actual);
+				Question003_1.DynamicSizedStack<int> sut0 = new Question003_1.DynamicSizedStack<int>(store, 0, initial0);
+				Question003_1.DynamicSizedStack<int> sut1 = new Question003_1.DynamicSizedStack<int>(store, 1, initial1);
+				Question003_1.DynamicSizedStack<int> sut2 = new Question003_1.DynamicSizedStack<int>(store, 2, initial2);
+				
+				int actual0 = sut0.Pop();
+				int actual1 = sut1.Pop();
+				int actual2 = sut2.Pop();
+				
+				Assert.AreEqual(expected0, actual0);
+				Assert.AreEqual(expected1, actual1);
+				Assert.AreEqual(expected2, actual2);
+			}		
+		}
+	}
+	
+}
